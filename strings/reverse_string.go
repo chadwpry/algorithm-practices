e59cf939b2ed9a689f0f62cfd8442e916329f70c
exit <chad.pry@gmail.com>
2024-10-06 16:33:07 -0500

    remove implementation for reversing a string

--- strings/reverse_string.go
+++ strings/reverse_string.go
@@ -1,21 +1,9 @@
 package algorithms
 
 func RecursiveReverse(input string) string {
-	runes := []rune(input)
-
-	if len(runes) <= 1 {
-		return input
-	}
-
-	return RecursiveReverse(string(runes[1:])) + string(runes[0])
+	return input
 }
 
 func ProceduralReverse(input string) string {
-	output := ""
-
-	for _, r := range input {
-		output = string(r) + output
-	}
-
-	return output
+	return input
 }
